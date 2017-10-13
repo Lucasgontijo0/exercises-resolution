f62e57a2685ffcd993a21961b060e1b9cce6db75
Lucas Gontijo <Lucasgontijo0@gmail.com>
2017-10-13 02:16:49 -0300

    1008 - Salário

---
+++ Python/1-Iniciante/1008.py
@@ -0,0 +1,8 @@
+employeeNumber = int(input())
+workedHours = int(input())
+hourValue = float(input())
+
+salary = workedHours * hourValue
+
+print('NUMBER = %d' % employeeNumber)
+print('SALARY = U$ %.2f' % salary)
