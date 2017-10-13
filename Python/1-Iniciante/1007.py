6483a49698da2f5e015b6f1f29d798884960cc6e
Lucas Gontijo <Lucasgontijo0@gmail.com>
2017-10-13 02:12:15 -0300

    1007 - Diferença

---
+++ Python/1-Iniciante/1007.py
@@ -0,0 +1,8 @@
+firstValue = int(input())
+secValue = int(input())
+thirdValue = int(input())
+fourthValue = int(input())
+
+prodDifference = firstValue * secValue - thirdValue * fourthValue
+
+print('DIFERENCA = %d' % prodDifference)
