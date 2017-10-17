bc3e1570d4904864a2884cdb1cda59a556c8169a
Lucas Gontijo <Lucasgontijo0@gmail.com>
2017-10-17 11:51:18 -0200

    1035 - Teste de Seleção 1

---
+++ Python/1-Iniciante/1035.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+line = map(float, input().split(" "))
+a, b, c, d = line
+
+if (b > c) and (d > a) and (c + d > a + b) and \
+   (c >= 0) and (d >= 0) and (a % 2 == 0):
+
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
