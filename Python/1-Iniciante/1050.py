71145731cb42a89911c1fa2a9f4d0ca7d0d7c4ce
Lucas Gontijo <Lucasgontijo0@gmail.com>
2017-10-19 11:34:36 -0200

    1050 - DDD

---
+++ Python/1-Iniciante/1050.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+ddds = {
+   61:'Brasilia',
+   71:'Salvador',
+   11:'Sao Paulo',
+   21:'Rio de Janeiro',
+   32:'Juiz de Fora',
+   19:'Campinas',
+   27:'Vitoria',
+   31:'Belo Horizonte'
+}
+
+value = int(input())
+
+if value in ddds:
+    print(ddds[value])
+else:
+    print('DDD nao cadastrado')
