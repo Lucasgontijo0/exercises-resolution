442014ca81b7ce4d3a7b9bee38932af996c4a7d1
Lucas Gontijo <Lucasgontijo0@gmail.com>
2017-10-20 13:09:52 -0200

    1052 - Mês

---
+++ Python/1-Iniciante/1052.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+months = []
+
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+month = int(input()) - 1
+print(months[month])
